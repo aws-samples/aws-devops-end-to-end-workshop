5623093fed991c9ea0d18bc32e74d8fa08f3ff73
Alexandre Nardi <44880405+alexandrenardi@users.noreply.github.com>
2023-04-12 19:02:30 -0300

    Update config.ts

--- assets/src/config.ts
+++ assets/src/config.ts
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: "us-east-1",
-    API_URL: "https://yuq7ws39pj.execute-api.us-east-1.amazonaws.com/prod",
+    API_URL: "https://o7bufxgoqb.execute-api.us-east-1.amazonaws.com/prod",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yA0vYd5AL",
-    APP_CLIENT_ID: "25048pu470diqqcvdo56i0cat9",
-    IDENTITY_POOL_ID: "us-east-1:1ff5a5f9-dd44-4934-b575-97d8d0952439"
+    USER_POOL_ID: "us-east-1_bn7eU7pVy",
+    APP_CLIENT_ID: "bqdp4f7updv4d41lqbc9qmgfm",
+    IDENTITY_POOL_ID: "us-east-1:a9680cc9-202c-4e28-9289-35d1b2fcebf0"
   }
 };
